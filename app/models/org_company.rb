8215b112988f922a8c942b440f838f712f80d07c
DennisDaddy <softteez95@gmail.com>
2017-04-30 17:05:53 +0300

    add associations

--- app/models/org_company.rb
+++ app/models/org_company.rb
@@ -2,4 +2,5 @@ class OrgCompany < ApplicationRecord
 	has_and_belong_to_many :org_contatcs
     has_many :org_persons
     has_many :org_products
+    belongs_to :typ_company, foreign_key: "typ_company_id"
 end
--- db/migrate/20170430135408_create_org_companies.rb
+++ db/migrate/20170430135408_create_org_companies.rb
@@ -1,6 +1,8 @@
 class CreateOrgCompanies < ActiveRecord::Migration[5.0]
   def change
     create_table :org_companies do |t|
+     t.string :name, null: false
+     t.belongs_to :typ_company
 
       t.timestamps
     end

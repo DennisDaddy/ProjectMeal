c86afa53bc87bb7687c21f9efbe0e8037e503666
DennisDaddy <softteez95@gmail.com>
2017-05-01 14:44:00 +0300

    generate migration

--- db/migrate/20170430135408_create_org_companies.rb
+++ db/migrate/20170430135408_create_org_companies.rb
@@ -2,6 +2,10 @@ class CreateOrgCompanies < ActiveRecord::Migration[5.0]
   def change
     create_table :org_companies do |t|
      t.string :name, null: false
+     t.string :avatar
+     t.text :description
+     t.string :verification_doc
+     t.boolean :verrified
      t.belongs_to :typ_company
 
       t.timestamps
